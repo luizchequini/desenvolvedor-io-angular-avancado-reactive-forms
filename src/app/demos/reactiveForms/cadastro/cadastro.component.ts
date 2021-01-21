d2abe0dae4f4e7d392d78c7e60d7f75e1ce26df7
Luiz Chequini <luizchequini@gmail.com>
2021-01-21 11:18:23 -0300

    02-Reactive Forms | 04 - Transformando o formulário em objetos tipados

--- src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Usuario } from './model/usuario';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,6 +9,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class CadastroComponent implements OnInit {
 
   cadastroForm: FormGroup;
+  usuario: Usuario;
 
   constructor(private fb: FormBuilder) { }
 
@@ -22,7 +24,7 @@ export class CadastroComponent implements OnInit {
   }
 
   adicionarUsuario(){
-    let x = this.cadastroForm.value;
+    this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
   }
 
 }
