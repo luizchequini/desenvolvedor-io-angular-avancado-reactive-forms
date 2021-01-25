af31170b6fce9d93846262b5a40296290323fe02
Luiz Chequini <luizchequini@gmail.com>
2021-01-25 19:11:45 -0300

    04-Roteamento Avançado | 04 - Links de navegação dinâmicos

--- src/app/navegacao/menu/menu.component.ts
+++ src/app/navegacao/menu/menu.component.ts
@@ -4,4 +4,51 @@ import { Component } from '@angular/core';
   selector: 'app-menu',
   templateUrl: './menu.component.html'
 })
-export class MenuComponent {}
+export class MenuComponent {
+
+  nav: Nav[] = [
+    {
+      link: '/home',
+      name: 'Home',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/cadastro',
+      name: 'Cadastro',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/produtos-dashboard',
+      name: 'Produto',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/sobre',
+      name: 'Sobre',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/contato',
+      name: 'Contato',
+      exact: true,
+      admin: false
+    },
+    {
+      link: '/feature',
+      name: 'Feature',
+      exact: true,
+      admin: false
+    }
+  ]
+}
+
+interface Nav{
+  link: string,
+  name: string,
+  exact: boolean,
+  admin: boolean
+}
