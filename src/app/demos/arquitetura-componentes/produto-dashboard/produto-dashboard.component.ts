4d8e70cc4c1c8cb5c4c8e3badc6f63bea411d125
Luiz Chequini <luizchequini@gmail.com>
2021-01-25 16:16:13 -0300

    03-Arquitetura de Componentes | 08 - ViewChildren - ViewChild decorators

--- src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
+++ src/app/demos/arquitetura-componentes/produto-dashboard/produto-dashboard.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { fromEvent, Observable } from 'rxjs';
+import { ProdutoDetalheComponent } from '../componentes/produto-card-detalhe.component';
+import { ProdutoCountComponent } from '../componentes/produto-count-component';
 import { Produto } from '../models/produto';
 
 @Component({
@@ -7,11 +10,31 @@ import { Produto } from '../models/produto';
   styles: [
   ]
 })
-export class ProdutoDashboardComponent implements OnInit {
+export class ProdutoDashboardComponent implements OnInit, AfterViewInit {
 
   ListaDeprodutos: Produto[];
 
+  @ViewChild(ProdutoCountComponent, {static: false}) contador: ProdutoCountComponent;
+  @ViewChild('teste', {static: false}) mensagemTela: ElementRef;
+
+  @ViewChildren(ProdutoDetalheComponent) botoes: QueryList<ProdutoDetalheComponent>;
+
   constructor() { }
+
+  ngAfterViewInit(): void {
+
+    console.log('Objeto do contado: ', this.contador.ListaDeprodutos);
+
+    let clickTexto: Observable<any> = fromEvent(this.mensagemTela.nativeElement, 'click');
+    clickTexto.subscribe(() =>{
+      alert('Clicou no texto!');
+      return;
+    })
+
+    console.log(this.botoes.forEach(p => {
+      console.log(p.produto);
+    }))
+  }
 
   ngOnInit() {
     this.ListaDeprodutos = 
